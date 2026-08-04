9784cd0264a1a1b246376f570e08b1af3d92a26b
Briar Lilly <silvermoon3497@gmail.com>
2026-08-03 23:01:45 -0500

    finishes fitting current design to an angular framework

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Menu } from './components/menu/menu';
+import { Events } from './components/events/events';
+import { AboutUs } from './components/about-us/about-us';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "home", pathMatch: "full"},
+    {path: "home", component: Home},
+    {path: "menu", component: Menu},
+    {path: "events", component: Events},
+    {path: "about-us", component: AboutUs}
+];
