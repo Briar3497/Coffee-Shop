e2086bd97494880069f332cfd841f4ec05b956fd
Briar Lilly <silvermoon3497@gmail.com>
2026-08-09 14:35:25 -0500

    adding database connection

--- backend/src/main/java/com/coffee_shop/backend/Entities/Product.java
+++ backend/src/main/java/com/coffee_shop/backend/Entities/Product.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
